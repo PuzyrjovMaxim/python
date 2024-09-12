0eaeb3e20d5a66c2605ff68919dd2c3ddbaa232f
PuzyrjovMaxim <113367308+PuzyrjovMaxim@users.noreply.github.com>
2024-09-13 00:27:11 +0300

    Add files via upload

---
+++ Cesar/main.py
@@ -0,0 +1,13 @@
+alph_lower = [chr(i) for i in range(ord('a'), ord('z')+1)]
+alph_upp = [chr(i) for i in range(ord('A'), ord('Z')+1)]
+n = input()
+s = input()
+m = list(s)
+for j in range(len(m)):
+    if m[j] in alph_lower:
+        i = (alph_lower.index(m[j]) + int(n)) % 26
+        m[j] = alph_lower[i]
+    elif m[j] in alph_upp:
+        i = (alph_upp.index(m[j]) + int(n)) % 26
+        m[j] = alph_upp[i]
+print(''.join(m))
